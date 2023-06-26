ae9202e47f6cb2d094c1866e815565932516f2b3
User <User@DESKTOP-4ROMB9B>
2023-06-26 19:30:06 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task__3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 2;
+            int userNumber;
+            bool compositeNumber = false;
+            Console.WriteLine("Введите число: ");
+            userNumber = int.Parse(Console.ReadLine());
+
+
+
+            while (n <= userNumber - 1) 
+            {
+                if (userNumber % n == 0)
+                {
+                    compositeNumber = true;
+                    break;
+                  
+                }
+                n++;
+            }
+            if (compositeNumber == false) 
+            {
+                Console.WriteLine("Число является простым.");
+
+            }
+            else 
+            {
+                Console.WriteLine("Число является составным.");
+            }
+                Console.ReadKey();
+
+
+                
+
+
+
+
+
+
+
+
+            
+        }
+    }
+}
